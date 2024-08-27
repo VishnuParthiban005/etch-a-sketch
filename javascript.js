8e55575a0c5941e0e567a479f9d24af1dc88462b
Vishnu <vishnupa@umich.edu>
2024-08-27 17:26:17 -0400

    Add javascript to make button functional
    Users can now press the button and enter a number to create a grid
    with that many squares on each side. The button is functional and it
    clears the current grid and creates a new one.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,30 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 16; ++i) {
-    const row_container = document.createElement("div");
-    container.appendChild(row_container);
-    row_container.style.display = "flex";
-    row_container.classList.add = "row" + i;
-    row_container.style.border = "2px solid black";
+btn.addEventListener("click", () => {
+    container.textContent = "";
+    let squares = prompt("Type the number of squares you would like on each side.");
+    drawSquares(parseInt(squares));
+});
 
-    for (let j = 0; j < 16; ++j) {
-        const column_container = document.createElement("div");
-        column_container.style.flex = "1 1 0%";
-        column_container.classList.add = "box" + i + j;
-        column_container.style.border = "2px dashed red";
-        column_container.addEventListener("mouseenter", () => {
-            column_container.style.backgroundColor = "red";
-        });
-        row_container.style.height = "90px";
-        row_container.appendChild(column_container);
+drawSquares();
 
+function drawSquares(squares=16) {
+    for (let i = 0; i < squares; ++i) {
+        const row_container = document.createElement("div");
+        container.appendChild(row_container);
+        row_container.style.display = "flex";
+        row_container.classList.add = "row" + i;
+    
+        for (let j = 0; j < squares; ++j) {
+            const column_container = document.createElement("div");
+            column_container.style.flex = "1 1 0%";
+            column_container.classList.add = "box" + i + j;
+            column_container.addEventListener("mouseenter", () => {
+                column_container.style.backgroundColor = "red";
+            });
+            row_container.style.height = (row_container.offsetWidth)/squares+ "px";
+            row_container.appendChild(column_container);
+        }
     }
-
 }
